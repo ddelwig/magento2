c9b974a8fd00f46aa10b4706fc846bcff9e5d8fa
Demian Delwig <demian.delwig@hypernode.com>
2026-03-31 15:56:32 +0200

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -4,7 +4,7 @@ namespace Hypernode\DeployConfiguration;
 
 $configuration = new ApplicationTemplate\Magento2(['en_US']);
 
-$productionStage = $configuration->addStage('production', 'magento2.komkommer.store');
-$productionStage->addServer('appname.hypernode.io');
+$productionStage = $configuration->addStage('production', 'hntestdemian.hypernode.io');
+$productionStage->addServer('hntestdemian.hypernode.io');
 
 return $configuration;
